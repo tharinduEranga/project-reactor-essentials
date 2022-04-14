b4cd90e59e6327a594a3914d96f873ddfc2181b5
Tharindu Eranga <95670397+tharinduwiley@users.noreply.github.com>
2022-04-14 14:47:47 +0530

    initialized the project

---
+++ src/test/java/com/tharindu/reactor/test/MonoTest.java
@@ -0,0 +1,13 @@
+package com.tharindu.reactor.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class MonoTest {
+
+    @Test
+    void test() {
+        log.info("Everything is working as expected!");
+    }
+}
