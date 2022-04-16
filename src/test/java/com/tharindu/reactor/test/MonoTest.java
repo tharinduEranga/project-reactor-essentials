382128441f945348a5c18be1d648100d72bb336f
Tharindu Eranga <95670397+tharinduwiley@users.noreply.github.com>
2022-04-16 14:32:47 +0530

    add some mono code

--- src/test/java/com/tharindu/reactor/test/MonoTest.java
+++ src/test/java/com/tharindu/reactor/test/MonoTest.java
@@ -2,12 +2,40 @@ package com.tharindu.reactor.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
+/*
+ * Reactive Streams
+ * 1. Asynchronous
+ * 2. Non-blocking
+ * 3. Backpressure
+ * Publisher <- (subscribe) Subscriber
+ * Subscription is created
+ * Publisher (onSubscribe with the subscription) -> Subscriber
+ * Subscription <- (request N)
+ * Publisher -> (onNext) Subscriber
+ * until:
+ * 1. Publisher sends all the objects requested.
+ * 2. Publisher sends all the objects it has (onComplete).
+ * 3. If there is an error. (onError) -> subscriber and subscription will be cancelled.
+ * */
 @Slf4j
 class MonoTest {
 
     @Test
-    void test() {
-        log.info("Everything is working as expected!");
+    void monoSubscriber() {
+        /*Logging a mono's content using inbuilt log*/
+        String name = "Tharindu Eranga";
+        Mono<String> mono = Mono.just(name)
+                .log();
+        mono.subscribe();
+
+        log.info("----------------------");
+
+        /*Logging a mono's content using test*/
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
     }
 }
